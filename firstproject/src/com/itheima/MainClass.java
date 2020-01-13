7412615eb595af5b353f3c3eb6f22a6242bbbebc
zhangsan <zhangsan@itcast.cn>
2020-01-13 13:45:21 +0800

    把本地工程添加到git

---
+++ firstproject/src/com/itheima/MainClass.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        System.out.println("111111");
+        System.out.println("111111");
+        System.out.println("111111");
+        System.out.println("111111");
+    }
+}
